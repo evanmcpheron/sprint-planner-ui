984d0fb6da8222bcf7d82aeec0adcbf582cbe7f6
Evan McPheron <evan.mcpheron@icloud.com>
2023-03-14 08:21:46 -0700

    working on package.json

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,2 +1,4 @@
 import openSocket from "socket.io-client";
-export const socket = openSocket("http://localhost:8080");
+export const socket = openSocket(
+  "https://sprint-planner-server.herokuapp.com/"
+);
